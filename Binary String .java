07ec62269eed4f892b0b88e546535bbf89a5668f
mukesh111 <66948644+mukesh61112@users.noreply.github.com>
2022-05-21 17:20:27 +0530

    Create Binary String .java

---
+++ Binary String .java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static Scanner sc = new Scanner(System.in);
+	public static void main (String[] args){
+        int t;
+    	t=sc.nextInt();
+    	sc.nextLine();
+    	while((t--)!=0){
+    	    int n=sc.nextInt();
+    	    sc.nextLine();
+    	    String s = new String();
+    	    s=sc.nextLine();
+    	    Solution obj = new Solution();
+    	    System.out.println(obj.binarySubstring(n, s));
+    	}
+	}
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to count the number of substrings that start and end with 1.
+    public static int binarySubstring(int a, String str)
+    {
+        // Your code here
+        int count=-1,sum=0;
+        for(int i=0;i<a;i++)
+        {
+             if(str.charAt(i)=='1')
+             {
+                 count++;
+                 sum=sum+count;
+             }
+        }
+        return sum;
+    }
+}
